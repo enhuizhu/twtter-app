762122db9886d481347db8a1dddbb109e55b3b7a
Enhui Zhu <enhuizhu@Enhuis-MacBook-Pro.local>
2020-03-03 22:42:13 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // 2 seconds
+  private heartBeat = 2000; 
+  
+  public state: any = {
+    feeds: [],
+  }
+  
+  componentDidMount() {
+    this.initFeeds();
+  }
+
+  initFeeds() {
+    this.getLatestFeeds().then(feeds => {
+      this.setState({
+        feeds
+      }, () => {
+        this.liveUpdate();
+      });
+    }).catch(e => {
+      setTimeout(this.initFeeds.bind(this), 100);
+    })
+  }
+
+  liveUpdate() {
+    setTimeout(async ()  => {
+      try {
+        let newFeeds = await this.getFutureFeeds(this.state.feeds[0].id);
+        newFeeds = [...newFeeds, ...this.state.feeds];
+        console.log('newFeeds', newFeeds);
+        this.setState({
+          feeds: newFeeds,
+        });  
+      } catch (e) {
+        console.log('error',  e);
+      }
+      
+      this.liveUpdate();
+      
+    }, this.heartBeat);
+  }
+
+  getLatestFeeds(): Promise<any[]> {
+    return fetch('https://magiclab-twitter-interview.herokuapp.com/enhuizhu/api?count=10')
+      .then(response => response.json())
+  }
+
+  getFutureFeeds(afterId: number) {
+    return fetch(`https://magiclab-twitter-interview.herokuapp.com/enhuizhu/api?count=1&id=${afterId}&direction=1`)
+      .then(response => response.json())
+  }
+  
+  render() {
+    console.log('feeds', this.state.feeds);
+    return (
+      <div className='App'>
+        {
+          this.state.feeds.map((f: any) => {
+            return (
+              <div className='item' key={f.id}>
+                <div className='avatar'>
+                  <img src={f.image}></img>
+                </div>
+                <div className='text-info'>
+                  <div className='username'>
+                    {f.username} &nbsp; {moment(f.timeStamp).startOf('hour').fromNow()}
+                  </div>
+                  <div className='feed-info'>
+                    {f.text}
+                  </div>
+                </div>
+                <div className='clear'></div>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
